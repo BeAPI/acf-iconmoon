8793b39b3564ce9b8aed4642712a845b3c0fbb35
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 16:15:15 +0200

    Update credentials

--- acf-iconmoon.php
+++ acf-iconmoon.php
@@ -1,16 +1,32 @@
 <?php
 /*
-Plugin Name: Advanced Custom Fields: Iconmoon
-Description: Add iconmoon selector
-Version: 0.2
-Author: Be API
-Author URI: www.beapi.fr
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: languages
-Text Domain: bea-acf-iconmoon
-*/
-
+ Plugin Name: Advanced Custom Fields: Iconmoon
+ Version: 0.2
+ Plugin URI: http://www.beapi.fr
+ Description: Add iconmoon selector
+ Author: BE API Technical team
+ Author URI: http://www.beapi.fr
+ Domain Path: languages
+ Text Domain: bea-acf-iconmoon
+ 
+ ----
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 class acf_field_iconmoon_plugin {
 	/**
